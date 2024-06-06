52737590678d02c9c43f34d0e5302108d833730c
BP <bhupi98.bs@gmail.com>
2024-06-06 15:22:31 -0700

    Actually working

--- src/modules/control_allocator/ActuatorEffectiveness/ActuatorEffectivenessTilts.cpp
+++ src/modules/control_allocator/ActuatorEffectiveness/ActuatorEffectivenessTilts.cpp
@@ -126,8 +126,8 @@ void ActuatorEffectivenessTilts::updateTorqueSign(const ActuatorEffectivenessRot
 		// Adding support for Roll control using tilting servos. Yaw is already supported so code above can be reused.
 		// _torque is a Vector. [roll scale, pitch scale, yaw scale, thrust scale]
 		if(_params[tilt_index].control == Control::YawAndRoll){
-			// how are these angles picked?
 			bool tilting_right = (int)_params[tilt_index].tilt_direction < 90 || (int)_params[tilt_index].tilt_direction > 270;
+			// if it's tilting right, set roll scale to 1
 			_torque[tilt_index](0) = tilting_right ? -1.f : 1.f;
 		}
 	}
